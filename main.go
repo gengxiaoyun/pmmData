43bb2a748fc0f8c801bd9fc8c8518eb91c45a0e1
gengxiaoyun <gengxiaoyun@shu.edu.cn>
2021-01-23 14:11:16 +0800

    commit

--- main.go
+++ main.go
@@ -6,83 +6,100 @@ import (
 	"os"
 	"fmt"
 	"log"
-	//"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
-	"bytes"
-	"io"
+	"io/ioutil"
+	simplejson "github.com/bitly/go-simplejson"
 )
 
-func Get(url string) (string,error) {
-
-	// 超时时间：5秒
-	client := &http.Client{Timeout: 15 * time.Second}
-	resp, err := client.Get(url)
-	if err != nil {
+func HttpProcess(url string) (string,error) {
+	req,err:=http.NewRequest("GET",url,nil)
+	req.SetBasicAuth("admin","123456")
+	if err!=nil{
 		return "",err
 	}
-	defer resp.Body.Close()
-	var buffer [512]byte
-	result := bytes.NewBuffer(nil)
-	for {
-		n, err := resp.Body.Read(buffer[0:])
-		result.Write(buffer[0:n])
-		if err != nil && err == io.EOF {
-			break
-		} else if err != nil {
-			return "",err
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp,err:=client.Do(req)
+
+	if err!=nil{
+		return "",err
+	}
+	body,err := ioutil.ReadAll(resp.Body)
+	if err!=nil {
+		return "",err
+	}
+	return string(body),nil
+}
+
+func JsonProcess(url string) error{
+	str,err:=HttpProcess(url)
+	if err!=nil{
+		return err
+	}
+	res,err := simplejson.NewJson([]byte(str))
+	if err != nil {
+		return err
+	}
+	result,err := res.Get("data").Get("result").Array()
+	if err != nil {
+		return err
+	}
+	for _, row := range result {
+		if each_map, ok := row.(map[string]interface{}); ok {
+			if eachMapMetric, ok := each_map["metric"].(map[string]interface{}); ok {
+				log.Printf("metric:{cpu=%s,mode=%s,node_name=%s}\n", eachMapMetric["cpu"],
+					eachMapMetric["mode"], eachMapMetric["node_name"])
+			}
+			if eachMapValues, ok := each_map["values"].([]interface{}); ok {
+				for i := 0; i < len(eachMapValues); i++ {
+					log.Printf("values[%d]:{timestamp=%s,value=%s}\n",i,eachMapValues[i].([]interface{})[0],
+						eachMapValues[i].([]interface{})[1])
+				}
+			}
+			if eachMapValue, ok := each_map["value"].([]interface{}); ok {
+				log.Printf("value:{timestamp=%s,value=%s}\n",eachMapValue[0],eachMapValue[1])
+			}
 		}
 	}
-
-	return result.String(),nil
+	return nil
 }
 
 func main() {
 	var (
-		//err error
-		logFileName = flag.String("./log", "InstallMysql.log", "Log file name")
+		err error
+		logFileName = flag.String("./log", "NodeExporter.log", "Log file name")
 	)
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.Parse()
 	//set logfile Stdout
+	_, err = os.Stat(*logFileName)
+	if err == nil {
+		err = os.Remove(*logFileName)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
 	logFile, logErr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 	if logErr != nil {
-		fmt.Println("Fail to find", *logFile, "InstallMysql start Failed")
+		fmt.Println("Fail to find", *logFile, "logFile Open Failed")
 		os.Exit(1)
 	}
 	log.SetOutput(logFile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	//write log
-	log.Printf("Log start! File:%v \n", "InstallMysql.log")
+	log.Printf("Log start! File:%v \n", "NodeExporter.log")
 
-	//r := gin.Default()
-	//r.GET("/prometheus/api/v1/query",func(c *gin.Context){
-	//	c.JSON(200,c.Query("query"))
-	//	//if err != nil{
-	//	//	log.Println(err.Error())
-	//	//}
-	//	//log.Println("AutoInStallMysql succeeded!")
-	//})
-	//r.Run(":80")
+	// query_range
+	log.Println("query=node_cpu_seconds_total, start=1611196430, end=1611196440, step=10\n")
+	err = JsonProcess("http://192.168.186.137/prometheus/api/v1/query_range?query=" +
+		"node_cpu_seconds_total{node_name=%22ubuntu%22}&" +
+		"start=1611196430&end=1611196440&step=10")
 
-	//resp,err := http.Get("http://192.168.186.137/prometheus/api/v1/query?query=node_disk_io_now")
-	//resp,err := http.Get("http://192.168.186.137/graph/api/datasources/proxy/1/api/v1/query_range?query=clamp_max(((avg%20by%20(mode)%20(%20(clamp_max(rate(node_cpu{instance%3D~%22localhost.localdomain%22%2Cmode!%3D%22idle%22}[5m])%2C1))%20or%20(clamp_max(irate(node_cpu{instance%3D~%22localhost.localdomain%22%2Cmode!%3D%22idle%22}[5m])%2C1))%20))*100%20or%20(avg_over_time(node_cpu_average{instance%3D~%22localhost.localdomain%22%2C%20mode!%3D%22total%22%2C%20mode!%3D%22idle%22}[5m])%20or%20avg_over_time(node_cpu_average{instance%3D~%22localhost.localdomain%22%2C%20mode!%3D%22total%22%2C%20mode!%3D%22idle%22}[5m])))%2C100)&start=1611169500&end=1611213000&step=300")
-	//
-	//if err!=nil{
-	//	log.Println(err.Error())
-	//}else{
-	//	body,err := ioutil.ReadAll(resp.Body)
-	//	if err!=nil {
-	//		log.Println(err.Error())
-	//	}
-	//	fmt.Println(string(body))
-	//}
+	// query
+	log.Println("query=node_cpu_seconds_total\n")
+	err = JsonProcess("http://192.168.186.137/prometheus/api/v1/query?query=node_cpu_seconds_total{node_name=%22ubuntu%22}")
 
-	result,err:=Get("http://192.168.186.137/prometheus/api/v1/query?query=node_disk_io_now")
-	if err!=nil {
+	if err!=nil{
 		log.Println(err.Error())
-	}else{
-		fmt.Println(result)
 	}
-
 }
